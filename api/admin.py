efaa834fac9c31a20b53a0e205e9353fa0cb1322
Pita Sangwani Zyambo <mightypz2@gmail.com>
2022-05-29 17:04:15 +0200

    change site title

--- api/admin.py
+++ api/admin.py
@@ -19,3 +19,4 @@ admin.site.register(Student, StudentAdmin)
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Term, TermAdmin)
 admin.site.register(Program, ProgramAdmin)
+admin.site.site_header = 'SkoolPay administration'
--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '27b2941e-c58e-413f-8f8e-d132fef39246'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -90,6 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "sfms_dev",
+        'USER': 'sangwani',
+        'PASSWORD': '200Hot@#2',
     }
 }
 
